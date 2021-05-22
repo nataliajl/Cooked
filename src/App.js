25b634f21bc028f0a6b6d2dc10cd77746418b721
Frederico Rappa <frerappa@gmail.com>
2021-05-22 00:38:15 -0300

    Criação da página de receita

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
 import HomeScreen from './screens/HomeScreen/HomeScreen';
+import RecipeScreen from './screens/RecipeScreen/RecipeScreen';
 import AddRecipeScreen from './screens/AddRecipeScreen/AddRecipeScreen';
 
 function App() {
   return (
     <div>
       <header className='App-header'>
-        <HomeScreen />
+        <RecipeScreen />
       </header>
     </div>
   );
