2bfe5cd86a8c465536bc97df17936b8aeb367310
Michell <lucinomichell@gmail.com>
2021-06-05 16:38:13 -0300

    Separação dos styles da navbar para apenas um componente

--- frontend/src/components/Navbar/NavbarStyles.js
+++ frontend/src/components/Navbar/NavbarStyles.js
@@ -1,7 +1,38 @@
-import { makeStyles } from '@material-ui/core';
+import { withStyles, Button } from '@material-ui/core';
+import ChevronRightRoundedIcon from '@material-ui/icons/ChevronRightRounded';
 
-export default makeStyles((theme) => ({
-  title: {
-    flexGrow: 1,
-  },
-}));
+
+export const StyledRightIcon = withStyles({
+    root: {
+        color: "#999999"
+    },
+  })(ChevronRightRoundedIcon);
+
+export const AccountButton = withStyles({
+    root: {
+        background: '#ffffff',
+        borderRadius: 6,
+        color: '#E43636',
+        height: 40,
+    },
+    label: {
+        textTransform: 'capitalize',
+        fontFamily: "Roboto",
+        fontSize: 16
+    },
+})(Button);
+
+export const SignInButton = withStyles({
+    root: {
+        background: 'linear-gradient(223.65deg, #E43636 12.5%, #F87E7E 111.77%)',
+        borderRadius: 6,
+        color: 'white',
+        height: 32,
+        width: 110.18
+    },
+    label: {
+        textTransform: 'capitalize',
+        fontFamily: "Roboto",
+        fontSize: 16
+    },
+})(Button);
