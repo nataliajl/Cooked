a5824644ff1ee88ab439bf08c4722e5f78c67c4a
Francisco <franciscogupe@gmail.com>
2021-06-26 17:13:22 -0300

    Create Recipe PATCH tests

---
+++ backend/src/shared/Tests/pathRecipe.spec.ts
@@ -0,0 +1,112 @@
+import "reflect-metadata"
+
+import '@shared/infra/typeorm';
+import "../container/index";
+import request from 'supertest';
+import app from '../infra/http/server';
+import { createConnection, getConnection } from 'typeorm';
+
+//Recipe model used in tests
+let createRecipeForm = {
+      "title": "Feijoada",
+      "description": "Feijoada de feijao bom",
+      "category": "brazilian",
+      "cookTime": 10,
+      "serves": 1,
+      "vegetarian": false,
+      "vegan": false,
+      "lactosefree": false,
+      "glutenfree": false,
+      "ingredients": [
+          {
+              "title": "Feijao",
+              "amount": 2
+          },
+          {
+              "title": "Porco",
+              "amount": 1
+          }     
+      ],
+      "private": false,
+      "steps": [
+          "Colocar no fogo",
+          "Colocar no prato",
+          "Comer"
+      ]
+}
+
+// Create Connection Before Tests 
+beforeAll(() => {
+  return createConnection();
+});
+
+// Close Connection Before Tests
+afterAll( async () => { 
+  let conn = getConnection();
+  return conn.close();
+});
+
+describe("Path Recipe Tests", () => {    
+
+  beforeEach(async () => {
+    await request(app)
+    .post('/recipes').send(createRecipeForm);
+  })
+
+  afterEach(async () => {
+    await request(app).delete('/recipes').send({title: "Feijoada"});
+  })
+
+  it("Should return status 200", async () => {
+
+    const res = await request(app)
+        .patch('/recipes').send(createRecipeForm);
+
+    expect(res.status).toBe(200);
+      
+})
+
+  it("Should successfully update modified recipe fields ", async () => {
+
+      createRecipeForm.category = "portuguese";
+      createRecipeForm.cookTime = 60;
+      
+      const res = await request(app)
+        .patch('/recipes').send(createRecipeForm);
+      
+      expect(res.body).toHaveProperty('category', 'portuguese');
+      expect(res.body).toHaveProperty('cookTime', 60);
+    })
+
+  it("Should not increase the Ingredients, but replace them ", async () => {
+    
+      createRecipeForm.ingredients = [
+        { "title": "Feijao preto ", "amount": 1}, 
+        {"title": "Calabresa", "amount": 3},
+        {"title": "Alho", "amount": 5}
+      ];
+
+      const res = await request(app)
+          .patch('/recipes').send(createRecipeForm);
+
+      expect(res.status).toBe(200);
+      expect(res.body.ingredients).toHaveLength(3);
+      expect(res.body.ingredients[0]).toEqual({ "title": "Feijao preto ", "amount": 1});
+
+  })
+
+  it("Should not increase the Steps, but replace them", async () => {
+
+      createRecipeForm.steps = [
+        "Comer",
+        "Comprar ingredients :("
+      ];
+
+      const res = await request(app)
+          .patch('/recipes').send(createRecipeForm);
+
+      expect(res.body.steps).toHaveLength(2);
+        
+  })
+
+});
--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -46,6 +46,4 @@ app.get('/', (req, res) => {
   return res.json({ Ping: 'Pong' });
 });
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333');
-});
+export default app
