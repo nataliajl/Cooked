c8a9a6c6e138acf561634714d7926c58603d7fb2
nataliajl <natalia.jleonardo@gmail.com>
2021-07-14 02:01:33 -0300

    Migration refactoring and fix getRecipeByIngredients API method

---
+++ backend/src/modules/ingredients/services/FindIngredientService.ts
@@ -0,0 +1,27 @@
+import { injectable, inject } from 'tsyringe';
+
+import IngredientsRepository from '../repositories/IIngredientsRepository';
+import Ingredient from '../infra/typeorm/entities/Ingredient';
+import Recipe from '@modules/recipes/infra/typeorm/entities/Recipe';
+
+@injectable()
+class FindIngredientService {
+
+  private ingredientsRepository: IngredientsRepository;
+
+  constructor(
+    @inject('IngredientsRepository')
+    ingredientsRepository: IngredientsRepository
+  ) {
+    this.ingredientsRepository = ingredientsRepository;
+  }
+
+  public async execute(
+    recipe: Recipe
+  ): Promise<Ingredient[]> {
+    
+    return await this.ingredientsRepository.findIngredientsByRecipe(recipe);
+  }
+}
+
+export default FindIngredientService;
